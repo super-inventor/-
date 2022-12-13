fd241548cb6fe5492d31d60f4c5d3d1e50021b5d
super-inventor <119829688+super-inventor@users.noreply.github.com>
2022-12-13 15:23:21 +0800

    Add files via upload

---
+++ 未命名1.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int F[1e4];
+int a[1e4];
+int P[1e4];
+int n;
+
+int erfen(int i,int n){
+	while(l +1!= r)
+	{
+		m=(l+r)/2;
+		if(m*m < x)
+			l=m;
+		else
+			r=m;
+		//cout<<m<<endl;
+	}
+}
+
+int main(){
+	cin>>n;
+	for(int i=1;i<=n;i++){
+		cin>>a[i];
+	}
+	for(int i=1;)
+	
+}
